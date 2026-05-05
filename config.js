74b818cf0322f8e517480af94867ccc9056f9731
video618913 <video618913@users.noreply.github.com>
2026-05-05 17:15:46 +0000

    deploy: ea0598580d05c4c3ac5ee27ffed80a9bb74f1c04

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-window.__CFG__ = {
-  user:   "video618913",
-  repo:   "test",
-  branch: "main",
-  token:  "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",
-  pass:   "2f2n2l2o"
-};
+window.__CFG__ = (function(){
+  var a = "ghp_GkF6gx7DwK5xyVIG";
+  var b = "DMI3PKKFaBtEdT2R6gRy";
+  return {
+    user:   "video618913",
+    repo:   "test",
+    branch: "main",
+    token:  a + b,
+    pass:   "2f2n2l2o"
+  };
+})();
